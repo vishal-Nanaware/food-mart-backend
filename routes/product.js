6a454c19b5c3a1fdc64028fd84f7c36198afabc1
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-07-01 23:12:29 +0530

    modify route for frontend

--- routes/product.js
+++ routes/product.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const data = require("../mockdata.json")
+const product = require("../models/product")
 
 
-router.post("/", (req,res)=>{
-    let quary = req.body.quary
 
-    res.json(data[quary])
-    console.log("send")
-})
+router.get("/",async (req, res) => {
+  const products =await product.find({})
+  res.json(products)
+});
+router.post("/ProductId", async (req, res) => {
+  const ProductId = req.query.ProductId;
+  
+  let item = await product.findById({ _id: ProductId });
+  res.json(item);
+});
 
-router.post('/id',(req,res)=>{
-    const id = req.query.id
-    console.log(typeof id)
-    let [category, pid] =[ Number(id.charAt(0)), Number(id.charAt(1))]
-    console.log(`category ${category} pid ${pid}`)
-    let product = data[category].find(item=> item.id === id)
-    // console.log(product)
-    res.json(product)
-})
+router.post("/category",async (req,res)=>{
+  const category = req.body.category;
+  try{
+    const products = await product.find({ productCategory : category});
+    res.json(products)
+  }catch(e){
+    console.log("error: ", e)
+  }
+
+});
 
 module.exports = router;
